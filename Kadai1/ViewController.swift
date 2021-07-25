41f057a06e0f710b1baa301cba0a72fb5d77be7a
Rin <rin.t.4921@gmail.com>
2021-07-26 01:00:24 +0900

    課題１完成

--- Kadai1/ViewController.swift
+++ Kadai1/ViewController.swift
@@ -8,12 +8,29 @@
 import UIKit
 
 class ViewController: UIViewController {
+    @IBOutlet private weak var textField1: UITextField!
+    @IBOutlet private weak var textField2: UITextField!
+    @IBOutlet private weak var textField3: UITextField!
+    @IBOutlet private weak var textField4: UITextField!
+    @IBOutlet private weak var textField5: UITextField!
+    @IBOutlet private weak var resultLabel: UILabel!
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        view.endEditing(true)
     }
 
+    @IBAction private func didTappedButton(_ sender: UIButton) {
+        let result = getSum()
+        resultLabel.text = String(result)
+    }
 
+    private func getSum() -> Int {
+        let num1 = Int(textField1.text ?? "") ?? 0
+        let num2 = Int(textField2.text ?? "") ?? 0
+        let num3 = Int(textField3.text ?? "") ?? 0
+        let num4 = Int(textField4.text ?? "") ?? 0
+        let num5 = Int(textField5.text ?? "") ?? 0
+        let result = num1 + num2 + num3 + num4 + num5
+        return result
+    }
 }
-
